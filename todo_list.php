1f38b4f30e42b79dd4d3ff02c14af6a3f4d0fe08
imaizumi <metallicaslayer2002@yahoo.co.jp>
2019-12-27 12:01:45 +0900

    todo_add

--- todo_list.php
+++ todo_list.php
@@ -5,5 +5,5 @@ $todo_list = read_todo_list(false);
 
 for ($i=0; $i < count($todo_list); $i++) { 
     $todo = $todo_list[$i];
-    echo $todo[0] . ":" . $todo[1] . "(" . $todo[2] .")" > PHP_EOL;
+    echo $todo[0] . ":" . $todo[1] . "(" . $todo[2] .")" . PHP_EOL;
 }
